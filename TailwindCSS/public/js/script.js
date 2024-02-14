286ac2443441e54127302159a615b4f6052335a8
youness404 <104984275+youness404@users.noreply.github.com>
2024-02-14 16:15:07 +0100

    Delete TailwindCSS directory

--- TailwindCSS/public/js/script.js
+++
@@ -1,9 +0,0 @@
-const mobileBtn = document.querySelector('#mobile-btn');
-const mobileMenu = document.querySelector('#mobile-menu');
-const burgerMenu = document.querySelector('#burger-menu');
-
-mobileBtn.addEventListener('click',()=>{
-    mobileMenu.classList.toggle('hidden');
-    
-    burgerMenu.classList.toggle('ri-xrp-line');
-});
